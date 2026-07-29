783abcde9ecb128e95e886987af5fb1b7700c8aa
SSSSSSSDsa <52715263+SSSSSSSDsa@users.noreply.github.com>
2026-07-29 20:06:11 +0000

    hopefullllyyyy fixed mor

--- js/routes.js
+++ js/routes.js
@@ -4,6 +4,6 @@ import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
+    { path: 'leaderboard', component: Leaderboard },
+    { path: 'roulette', component: Roulette },
 ];
